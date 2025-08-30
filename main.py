217e6cabd789609f37e172d269bde40e2f989e36
jakubvod <vodicka.microsoft@seznam.cz>
2025-08-30 17:21:16 +0200

    Change whole app (now it's for spotify stats) + add client_id and secret to .env

--- main.py
+++ main.py
@@ -1,9 +1,31 @@
-from flask import Flask, render_template, Blueprint, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash
+from collections import Counter
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
-import os
+from spotipy.cache_handler import FlaskSessionCacheHandler
+from dotenv import load_dotenv
+import os, random
 
 app = Flask(__name__)
+app.secret_key = os.urandom(64)
+
+load_dotenv()
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
+REDIRECT_URI = os.environ.get("REDIRECT_URI")
+SCOPE = 'user-top-read'
+
+# Takes care of tokens
+cache_handler = FlaskSessionCacheHandler(session)
+
+oauth = SpotifyOAuth(
+    client_id=CLIENT_ID, 
+    client_secret=CLIENT_SECRET, 
+    redirect_uri=REDIRECT_URI, scope=SCOPE, 
+    cache_handler=cache_handler,
+    show_dialog=True
+)
+sp = Spotify(oauth_manager=oauth)
 
 @app.route("/")
 def home():
@@ -11,7 +33,35 @@ def home():
 
 @app.route("/auth")
 def auth():
-    return render_template("auth.html")
+    if not oauth.validate_token(cache_handler.get_cached_token()):
+        return redirect(oauth.get_authorize_url())
+    return redirect(url_for('get_stats'))
+
+@app.route("/callback")
+def callback():
+   oauth.get_access_token(request.args['code'])
+   return redirect(url_for('get_stats'))
+
+@app.route("/get_stats")
+def get_stats():
+    if not oauth.validate_token(cache_handler.get_cached_token()):
+        return redirect(oauth.get_authorize_url())
+
+    top_artists = sp.current_user_top_artists(limit=30, time_range='short_term')["items"]
+    top_tracks = sp.current_user_top_tracks(limit=30, time_range='short_term')["items"]
+
+    if len(top_artists) != 0 and len(top_tracks) != 0:
+        top_artists_names = [top_artist["name"] for top_artist in top_artists]
+        top_tracks_names = [top_track["name"] for top_track in top_tracks]
+
+        genres_counter: dict[str, int] = {}
+        for top_artist in top_artists:
+            for genre in top_artist["genres"]:
+                genres_counter[genre] = genres_counter.get(genre, 0) + 1
+        genres_counter = Counter(genres_counter)
+        top_5_genres = genres_counter.most_common(5)
+        genres_result = [genre for genre, count in top_5_genres]
+    return render_template("get_stats.html", top_artists=top_artists_names, top_tracks=top_tracks_names, top_genres=genres_result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8888)
