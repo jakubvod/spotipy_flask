1a2a9f26ff77edab0a1d5e5ddf5365d2e959f855
jakubvod <vodicka.microsoft@seznam.cz>
2025-08-30 12:46:12 +0200

    changehome page

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, Blueprint, redirect, url_for, request, session, flash
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/auth")
+def auth():
+    return "DONE!"
 
 if __name__ == "__main__":
     app.run(debug=True)
